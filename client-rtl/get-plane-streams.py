eae9283d4e7a825a265e7f2db6534af5b887457a
Dirk Grunwald <grunwald@colorado.edu>
2022-07-23 20:55:48 -0600

    add example retrieving streams

---
+++ client-rtl/get-plane-streams.py
@@ -0,0 +1,41 @@
+import asyncio
+import os, sys
+
+import nats
+from nats.errors import TimeoutError
+
+##
+## An example of subcribing to a consumer to retrieve older data
+##
+
+durable = False
+
+async def main():
+    token = os.getenv("TOKEN")
+    if not token:
+        print("You need to define TOKEN")
+        sys.exit(1)
+    nc = await nats.connect(f"nats://{token}@198.59.70.45:8080")
+
+    # Create JetStream context incase it's not defined
+    js = nc.jetstream()
+    await js.add_stream(name="planes", subjects=["plane.*"])
+
+    for topic in [ "reporter", "ident", "loc" ]:
+        print("\n+++++")
+        print(f"Topic: plane-{topic}")
+        if durable:
+            sub = await js.subscribe(f"plane.{topic}", durable=f"durable-{topic}", ordered_consumer=True)
+        else:
+            sub = await js.subscribe(f"plane.{topic}", ordered_consumer=True)
+        while True:
+            try:
+                msg = await sub.next_msg()
+                print("message is ", msg)
+            except TimeoutError:
+                break
+    await nc.close()
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
