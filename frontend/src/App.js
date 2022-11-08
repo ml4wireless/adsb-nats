0ea1c029381fc05e26314275c65bbd7fd909ea11
weijili <weijili@umich.edu>
2022-11-07 16:14:56 -0800

    add different colors to markers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,45 +22,52 @@ const Marker = ({ onClick, children, feature }) => {
 
 const App = () => {
   const mapContainerRef = useRef(null);
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [colorMap, setColorMap] = useState(new Map());
 
   useEffect(() => {
     axios.get('http://ec2-35-80-21-70.us-west-2.compute.amazonaws.com:5000/getJsonStream')
       .then(res => {
         let data = res.data.filter((dataItem) => ("lon" in dataItem && "lat" in dataItem));
-        data = data.slice(0, 100);
+        // data = data.slice(0, 100);
         setData(data);
-        console.log(data);
 
         const map = new mapboxgl.Map({
           container: mapContainerRef.current,
           style: "mapbox://styles/mapbox/streets-v11",
-          center: [-122.492961, 37.729425],
-          zoom: 10,
+          center: [-122.3, 37.8],
+          zoom: 9,
         });
     
         // Render custom marker components
         data.forEach((dataItem) => {
-          // Create a React ref
-
-          
-         /** 
-          const ref = React.createRef();
-          // Create a new DOM node and save it to the React ref
-          ref.current = document.createElement("div");
-          // Render a Marker Component on our new DOM node
-          
-          ReactDOM.render(
-            <Marker onClick={markerClicked} feature={feature} />,
-            ref.current
-          );
-          */
-
           console.log(dataItem);
-          console.log([dataItem["lon"], dataItem["lat"]]);
-    
+          
+
+          // determine color
+          var color;
+          if (colorMap.has(dataItem["icao"])){
+            color = colorMap.get(dataItem["icao"]);
+          } else {
+            var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+            colorMap.set(dataItem["icao"], randomColor);
+            setColorMap(colorMap);
+            color = colorMap.get(dataItem["icao"]);
+          }
+
+          // create a parent class
+          const el = document.createElement('div');
+          el.className = 'marker';
+          el.style.backgroundColor = color;
+
           // Create a Mapbox Marker at our new DOM node
-          new mapboxgl.Marker()
+          new mapboxgl.Marker(el)
+            .setPopup(
+              new mapboxgl.Popup({ offset: 25 }) // add popups
+              .setHTML(
+                `<h3>${dataItem["icao"]}</h3>`
+              )
+            )
             .setLngLat([dataItem["lon"], dataItem["lat"]])
             .addTo(map);
         });
