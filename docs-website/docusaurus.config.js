951df420d55b6e43126d78115d6bf32eb6bef493
weijili <weijili@umich.edu>
2022-11-14 15:56:16 -0800

    change baseurl

--- docs-website/docusaurus.config.js
+++ docs-website/docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  baseUrl: '/adsb-nats/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
