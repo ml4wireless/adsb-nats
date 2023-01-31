0c602ec26f19c5fb76cfcc0b00537037a950b591
vsiv16 <55516887+vsiv16@users.noreply.github.com>
2023-01-30 16:48:21 -0800

    add monitoring port to nats_service.yml

--- aircraft-annotator/run-docker.sh
+++ aircraft-annotator/run-docker.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-#copied from nhttps://github.com/ml4wireless/adsb-nats/blob/1ec407770eaef76e089661c82a303f39892fa0a7/client-rtl/run-docker.sh
-#and then edited 
+########## AWS ##########
+export NATS_HOST="a15d11836d0644f6da0d09cbd81fae4f-949e37ea0352e6ad.elb.us-west-2.amazonaws.com:4222"
+export TOKEN="sahai"
 
-##
-## Define 
-export NATS_HOST=${NATS_HOST:-"docker.for.mac.host.internal:63791"}
-export TOKEN=${TOKEN:-"shruti"}
+########## LOCAL ##########
+# export NATS_HOST=${NATS_HOST:-"docker.for.mac.host.internal:51004"} 
+# export TOKEN=${TOKEN:-"sivanitoken"}
 
-docker run --network "host" --privileged \
+######################################################################
+docker run --network "host" --privileged -v /dev/bus/usb/dev/bus/usb \
        -e NATS_HOST=${NATS_HOST} \
        -e TOKEN=${TOKEN} \
        nats-annotator:latest
-       
