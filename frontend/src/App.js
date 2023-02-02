dfabde3c0eb148c84f88b66d6c7b01cc1b604b01
Jz1116 <vjiang1116@outlook.com>
2023-02-02 11:59:40 -0800

    fix ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,59 +11,58 @@ const App = () => {
   const [colorMap, setColorMap] = useState(new Map());
 
   useEffect(() => {
-    setInterval(() => {
-      axios.get('https://cors-everywhere.herokuapp.com/http://ec2-35-80-21-70.us-west-2.compute.amazonaws.com:5000/getJsonStream')
-      .then(res => {
-        let data = res.data.filter((dataItem) => ("lon" in dataItem && "lat" in dataItem));
-        setData(data);
+    axios.get('http://ec2-35-80-21-70.us-west-2.compute.amazonaws.com:5000/getJsonStream?start_date=2023-01-31%2000:11:11&&end_date=2023-01-31%2000:25:11')
+    .then(res => {
+      let data = res.data.filter((dataItem) => ("lon" in dataItem && "lat" in dataItem));
+      data = data.slice(0, 100);
+      setData(data);
 
-        const map = new mapboxgl.Map({
-          container: mapContainerRef.current,
-          style: "mapbox://styles/mapbox/streets-v11",
-          center: [-122.3, 37.8],
-          zoom: 9,
-        });
-    
-        // Render custom marker components
-        data.forEach((dataItem) => {
-          console.log(dataItem);
-          
+      const map = new mapboxgl.Map({
+        container: mapContainerRef.current,
+        style: "mapbox://styles/mapbox/streets-v11",
+        center: [-122.3, 37.8],
+        zoom: 9,
+      });
+  
+      // Render custom marker components
+      data.forEach((dataItem) => {
+        console.log(dataItem);
+        
 
-          // determine color
-          var color;
-          if (colorMap.has(dataItem["ICAO"])){
-            color = colorMap.get(dataItem["ICAO"]);
-          } else {
-            var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-            colorMap.set(dataItem["ICAO"], randomColor);
-            setColorMap(colorMap);
-            color = colorMap.get(dataItem["ICAO"]);
-          }
+        // determine color
+        var color;
+        if (colorMap.has(dataItem["ICAO"])){
+          color = colorMap.get(dataItem["ICAO"]);
+        } else {
+          var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+          colorMap.set(dataItem["ICAO"], randomColor);
+          setColorMap(colorMap);
+          color = colorMap.get(dataItem["ICAO"]);
+        }
 
-          // create a parent class
-          const el = document.createElement('div');
-          el.className = 'marker';
-          el.style.backgroundColor = color;
+        // create a parent class
+        const el = document.createElement('div');
+        el.className = 'marker';
+        el.style.backgroundColor = color;
 
-          // Create a Mapbox Marker at our new DOM node
-          new mapboxgl.Marker(el)
-            .setPopup(
-              new mapboxgl.Popup({ offset: 25 }) // add popups
-              .setHTML(
-                `<h3>${dataItem["ICAO"]}</h3>`
-              )
+        // Create a Mapbox Marker at our new DOM node
+        new mapboxgl.Marker(el)
+          .setPopup(
+            new mapboxgl.Popup({ offset: 25 }) // add popups
+            .setHTML(
+              `<h3>${dataItem["ICAO"]}</h3>`
             )
-            .setLngLat([dataItem["lon"], dataItem["lat"]])
-            .addTo(map);
-        });
-    
-        // Add navigation control (the +/- zoom buttons)
-        map.addControl(new mapboxgl.NavigationControl(), "top-right");
-    
-        // Clean up on unmount
-        return () => map.remove(); 
-      })
-    }, 60000)
+          )
+          .setLngLat([dataItem["lon"], dataItem["lat"]])
+          .addTo(map);
+      });
+  
+      // Add navigation control (the +/- zoom buttons)
+      map.addControl(new mapboxgl.NavigationControl(), "top-right");
+  
+      // Clean up on unmount
+      return () => map.remove(); 
+    })
   }, [data])
 
   return <div className="map-container" ref={mapContainerRef} />;
