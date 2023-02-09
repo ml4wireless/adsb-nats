8bd425d995a3dbe936c0702f1681183e9b7db149
vsiv16 <55516887+vsiv16@users.noreply.github.com>
2023-02-08 16:57:35 -0800

    for testing containerized version of client

---
+++ client-rtl-docker/planes-purge.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This assumes you have the nat CLI installed - https://github.com/nats-io/natscli/releases/tag/v0.0.33
+#
+if [[ -z "$NATS_HOST" ]]; then
+    echo "Using NATS host $NATS_HOST"
+else
+    NATS_HOST="localhost:30303"
+    echo "Using NATS host $NATS_HOST"
+fi
+
+nats -s nats://"$TOKEN"@"$NATS_HOST" stream purge plane
---
+++ client-rtl-docker/run-docker.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+##
+## Define 
+export NATS_HOST=${NATS_HOST:-"a15d11836d0644f6da0d09cbd81fae4f-949e37ea0352e6ad.elb.us-west-2.amazonaws.com:4222"}
+export NATS_TOKEN=${NATS_TOKEN:-"<insert token here>"}
+
+docker run -it --privileged -v /dev/bus/usb/dev/bus/usb \
+       -e NATS_HOST=${NATS_HOST} \
+       -e NATS_TOKEN=${NATS_TOKEN} \
+       adsb-nats-client:latest 
