556c8c1de41d07d1476fe03ce95f88651a11bb22
Jz1116 <vjiang1116@outlook.com>
2023-02-09 00:01:18 -0800

    add search based on time

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,70 +2,181 @@ import './App.css';
 import React, { useRef, useEffect, useState } from 'react';
 import axios from 'axios';
 import mapboxgl from '!mapbox-gl';
+import DateTimeRangePicker from '@wojtekmaj/react-datetimerange-picker'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiemppYW5nMzMwIiwiYSI6ImNsOXhhdzhiMDA4eG8zb21qbHkwbXdrdTcifQ.LGmuZP4-Pekk3ht0JuU6oQ';
+// look aa past 30 seconds
+const PAST_TIME = 30 * 1000;
+const TIME_LIMIT = 120 * 1000;
+const Modes = {
+	REAL_TIME: "real_time",
+	CHOOSE_TIME: "choose_time",
+}
 
 const App = () => {
+  const map = useRef(null);
   const mapContainerRef = useRef(null);
-  const [data, setData] = useState([]);
   const [colorMap, setColorMap] = useState(new Map());
+  const [dates, setDates] = useState([null, null])
+  const [mode, setMode] = useState(Modes.REAL_TIME);
+  const [markers, setMarkers] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://ec2-35-80-21-70.us-west-2.compute.amazonaws.com:5000/getJsonStream?start_date=2023-01-31%2000:11:11&&end_date=2023-01-31%2000:25:11')
+  const handleSubmit = () => {
+    const [startDate, endDate] = dates;
+
+    if (endDate.getTime() - startDate.getTime() >= TIME_LIMIT) {
+      alert("Please select time frame less than 2 minutes");
+      return
+    }
+
+    markers.forEach(marker => marker.remove());
+    setMarkers([]);
+    setMode(Modes.CHOOSE_TIME);
+    getFlightDataWithArgs();
+  }
+
+  const handleReset = () => {
+    markers.forEach(marker => marker.remove());
+    setMarkers([]);
+    setDates([null, null]);
+    setMode(Modes.REAL_TIME);
+    getFlightData();
+  }
+
+  const getTimeParam = (timeDate) => {
+    const [date, timeInfo] = timeDate.toISOString().split("T");
+    const time = timeInfo.split(".")[0];
+    const param = date + "%20" + time;
+    return param;
+  }
+
+  const getFlightRequest = (url) => {
+    axios.get(url)
     .then(res => {
       let data = res.data.filter((dataItem) => ("lon" in dataItem && "lat" in dataItem));
-      data = data.slice(0, 100);
-      setData(data);
-
-      const map = new mapboxgl.Map({
-        container: mapContainerRef.current,
-        style: "mapbox://styles/mapbox/streets-v11",
-        center: [-122.3, 37.8],
-        zoom: 9,
-      });
-  
+      const markersTemp = [];
       // Render custom marker components
       data.forEach((dataItem) => {
-        console.log(dataItem);
-        
-
         // determine color
         var color;
-        if (colorMap.has(dataItem["ICAO"])){
-          color = colorMap.get(dataItem["ICAO"]);
+        const icao = dataItem["ICAO"].toUpperCase();
+        if (colorMap.has(icao)){
+          color = colorMap.get(icao);
         } else {
           var randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
-          colorMap.set(dataItem["ICAO"], randomColor);
+          colorMap.set(icao, randomColor);
           setColorMap(colorMap);
-          color = colorMap.get(dataItem["ICAO"]);
+          color = colorMap.get(icao);
         }
-
+        
         // create a parent class
         const el = document.createElement('div');
         el.className = 'marker';
         el.style.backgroundColor = color;
 
+        const time = new Date(dataItem.time);
+        const dateString = time.toLocaleDateString();
+        const timeString = time.toLocaleTimeString();
+
         // Create a Mapbox Marker at our new DOM node
-        new mapboxgl.Marker(el)
+        const marker = new mapboxgl.Marker(el)
           .setPopup(
-            new mapboxgl.Popup({ offset: 25 }) // add popups
+            new mapboxgl.Popup({ offset: 10 }) // add popups
             .setHTML(
-              `<h3>${dataItem["ICAO"]}</h3>`
+              `
+              <div>
+                <p>ICAO: ${icao}</p>
+                <p>time: ${dateString + ' ' + timeString}</p>
+              </div>
+              `
             )
           )
           .setLngLat([dataItem["lon"], dataItem["lat"]])
-          .addTo(map);
+          .addTo(map.current);
+          markersTemp.push(marker);
       });
-  
-      // Add navigation control (the +/- zoom buttons)
-      map.addControl(new mapboxgl.NavigationControl(), "top-right");
-  
+      setMarkers(markersTemp);
       // Clean up on unmount
-      return () => map.remove(); 
-    })
-  }, [data])
+      return () => map.current.remove(); 
+    })    
+  }
 
-  return <div className="map-container" ref={mapContainerRef} />;
+  const getFlightData = () => {
+    const now = Date.now();
+    const startTime = getTimeParam(new Date(now - PAST_TIME));
+    const endTime = getTimeParam(new Date(now));
+    const url = `https://ec2-35-80-21-70.us-west-2.compute.amazonaws.com/getJsonStream?start_date=${startTime}&&end_date=${endTime}`;
+    getFlightRequest(url);
+  }
+
+  const getFlightDataWithArgs = () => {
+    const [startDate, endDate] = dates;
+
+    const startTime = getTimeParam(startDate);
+    const endTime = getTimeParam(endDate);
+    const url = `https://ec2-35-80-21-70.us-west-2.compute.amazonaws.com/getJsonStream?start_date=${startTime}&&end_date=${endTime}`;
+    getFlightRequest(url);
+  }
+
+
+  useEffect(() => {
+    if (map.current) return;
+    map.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [-122.3, 37.8],
+      zoom: 9,
+    });
+    map.current.addControl(new mapboxgl.NavigationControl(), "top-right");
+    getFlightData();
+  }, [])
+
+  useEffect(() => {
+    if (mode == Modes.REAL_TIME) {
+      const interval = setInterval(() => {
+        markers.forEach(marker => marker.remove());
+        setMarkers([]);
+        getFlightData();
+      }, PAST_TIME);
+      return () => clearInterval(interval);
+    }
+  }, [mode, markers]);
+
+  return (
+    <div className="container">
+      <div className="form">
+          {mode === Modes.REAL_TIME? (
+            <div>
+              data update every {PAST_TIME / 1000} seconds
+            </div>
+          ): (
+            <div>
+              no data update in this mode
+            </div>
+          )}
+        <DateTimeRangePicker 
+          value={dates}
+          onChange={setDates}
+          locale="en-EN"
+          className="bg-w"
+        />
+        <br />
+        <button 
+          onClick={handleSubmit} 
+          disabled={dates[0] === null || dates[1] === null}
+        >
+          submit
+        </button>
+        <button 
+          onClick={handleReset} 
+          disabled={dates[0] === null || dates[1] === null}
+        >
+          reset
+        </button>
+      </div>
+      <div className="map-container" ref={mapContainerRef} />
+    </div>
+    );
 };
 
 export default App;
