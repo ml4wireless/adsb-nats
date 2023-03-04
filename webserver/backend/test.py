72f7bd0d535af3f124a1ed33277d64eeebc2b5a5
EC2 Default User <ec2-user@ip-192-168-48-31.us-west-2.compute.internal>
2023-03-04 14:30:12 +0000

    --no-edit

--- webserver/backend/test.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime, timedelta, timezone
-
-timezone_offset = -8.0  
-tzinfo = timezone(timedelta(hours=timezone_offset))
-now = datetime.now(tzinfo)
-retention_time =  (now -  timedelta(days=20)).strftime("%Y-%m-%d %H:%M:%S.000000")
-print(f"data in retention before {retention_time}")
