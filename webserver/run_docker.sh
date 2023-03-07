98932612b84835c96782ec194912f5a35fd5c645
sjtu-biggie <zhull991122@sjtu.edu.cn>
2023-03-07 00:21:07 +0000

    delete extra webserver files

--- webserver/run_docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-## Define 
-NATS_HOST=${NATS_HOST}
-TOKEN=${TOKEN}
-docker run -d --privileged \
-       -e NATS_HOST=${NATS_HOST} \
-       -e TOKEN=${TOKEN} \
-       --network "host" \
-       aircraft-webserver:latest
